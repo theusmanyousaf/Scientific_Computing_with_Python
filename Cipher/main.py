bc8f0d5b1cba7e637f3def99c1025b9f698fed6f
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-16 23:19:56 +0500

    shift logic

--- Cipher/main.py
+++ Cipher/main.py
@@ -1,6 +1,7 @@
 text = 'Hello World' # text variable of type 'str'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0])
+index = alphabet.find(text[0].lower())
 print(index)  # print is a built in function to see output on console
-print(text.lower())
+shifted = alphabet[index + shift]
+print(shifted)
