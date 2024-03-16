1ebcb81119df1b3e69dfbadc1ec450f71281d459
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-16 23:52:13 +0500

    for loop

--- Cipher/main.py
+++ Cipher/main.py
@@ -1,7 +1,8 @@
 text = 'Hello World' # text variable of type 'str'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-print(index)  # print is a built in function to see output on console
-shifted = alphabet[index + shift]
-print(shifted)
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
