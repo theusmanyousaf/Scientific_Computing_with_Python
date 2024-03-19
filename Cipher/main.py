97566c560a0d67ae6b5c4e763fb3dad9f1cb5e50
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-19 23:58:04 +0500

    vigenere encryption & decryption

--- Cipher/main.py
+++ Cipher/main.py
@@ -1,28 +1,30 @@
 text = 'Hello Zaira' # text variable of type 'str'
 custom_key = 'python'
 
-def vigenere(message, key):
+def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
     
         # Append space to the message
         if char == ' ':
-            encrypted_text += char
+            final_message += char
         else:        
-            # Find the right key character to encode
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted letter
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
     
-    return encrypted_text
+    return final_message
     
 encryption = vigenere(text, custom_key)
 print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
