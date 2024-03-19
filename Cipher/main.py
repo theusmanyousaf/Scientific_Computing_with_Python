2ff9912ca50d5888a2f0f78e3e87ad9fcdaf29c7
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-19 23:55:58 +0500

    vigenere encryption

--- Cipher/main.py
+++ Cipher/main.py
@@ -1,18 +1,28 @@
 text = 'Hello Zaira' # text variable of type 'str'
-shift = 3
+custom_key = 'python'
 
-def caesar(message, offset):
+def vigenere(message, key):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
+    
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
-        else:
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
-
-caesar(text, shift)
+    
+    return encrypted_text
+    
+encryption = vigenere(text, custom_key)
+print(encryption)
