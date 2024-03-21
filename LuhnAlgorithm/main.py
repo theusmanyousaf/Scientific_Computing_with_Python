e61973db4640d99d45c2c730e30f98f80e5a9a5d
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-21 21:49:15 +0500

    luhn algo verification

--- LuhnAlgorithm/main.py
+++ LuhnAlgorithm/main.py
@@ -13,13 +13,17 @@ def verify_card_number(card_number):
         if number >= 10:
             number = (number // 10) + (number % 10)
         sum_of_even_digits += number
-    
-    
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
+
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
-    verify_card_number(translated_card_number)
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
 
 main()
