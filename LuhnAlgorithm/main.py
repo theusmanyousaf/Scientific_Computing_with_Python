ba720e90daca48ef119d51b5ec515374276dac1a
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-21 21:47:36 +0500

    even digits luhn logic

--- LuhnAlgorithm/main.py
+++ LuhnAlgorithm/main.py
@@ -4,7 +4,16 @@ def verify_card_number(card_number):
     odd_digits = card_number_reversed[::2]
 
     for digit in odd_digits:
-        print(digit)
+        sum_of_odd_digits += int(digit)
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+    
     
 def main():
     card_number = '4111-1111-4555-1142'
