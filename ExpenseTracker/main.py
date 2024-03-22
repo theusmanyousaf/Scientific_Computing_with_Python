f706d1ac985e611a436cd74a61358cec371445e2
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-22 22:46:56 +0500

    expenses ui func

--- ExpenseTracker/main.py
+++ ExpenseTracker/main.py
@@ -1,4 +1,8 @@
 def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
 
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
 expenses = []
