9355ec8db20ef0aa9353edcb246fc66ddc585b03
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-22 22:48:17 +0500

    total expenses lambda func

--- ExpenseTracker/main.py
+++ ExpenseTracker/main.py
@@ -5,4 +5,7 @@ def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 
+def total_expenses(expenses):
+    return sum(map(lambda expense: expense['amount'], expenses))
+
 expenses = []
