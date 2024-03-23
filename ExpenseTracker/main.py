94de303b97049196401fa4cd0442508072def159
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-23 23:03:31 +0500

    user input

--- ExpenseTracker/main.py
+++ ExpenseTracker/main.py
@@ -20,3 +20,27 @@ def main():
         print('3. Show total expenses')
         print('4. Filter expenses by category')
         print('5. Exit')
+        
+        choice = input('Enter your choice: ')
+
+        if choice == '1':
+            amount = float(input('Enter amount: '))
+            category = input('Enter category: ')
+            add_expense(expenses, amount, category)
+
+        elif choice == '2':
+            print('\nAll Expenses:')
+            print_expenses(expenses)
+
+        elif choice == '3':
+            print('\nTotal Expenses: ', total_expenses(expenses))
+
+        elif choice == '4':
+            category = input('Enter category to filter: ')
+            print(f'\nExpenses for {category}:')
+            expenses_from_category = filter_expenses_by_category(expenses, category)
+            print_expenses(expenses_from_category)
+
+        elif choice == '5':
+            print('Exiting the program.')
+            break
