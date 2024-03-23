28a36ab76e4bba77786c441e413328b1f9baafaa
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-23 22:59:13 +0500

    expenses filter

--- ExpenseTracker/main.py
+++ ExpenseTracker/main.py
@@ -8,4 +8,7 @@ def print_expenses(expenses):
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
 
+def filter_expenses_by_category(expenses, category):
+    return filter(lambda expense: expense['category'] == category, expenses)
+
 expenses = []
