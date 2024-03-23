94db3be92e3ccf5236c15b3987fc84eea6a3afe1
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-23 23:00:47 +0500

    main func

--- ExpenseTracker/main.py
+++ ExpenseTracker/main.py
@@ -11,4 +11,12 @@ def total_expenses(expenses):
 def filter_expenses_by_category(expenses, category):
     return filter(lambda expense: expense['category'] == category, expenses)
 
-expenses = []
+def main():
+    expenses = []
+    while True:
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
