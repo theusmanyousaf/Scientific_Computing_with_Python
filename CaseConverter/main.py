06a5fbf8b1425eadd382abaa54593ad8b25634fa
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-26 22:58:45 +0500

    conversion logic

--- CaseConverter/main.py
+++ CaseConverter/main.py
@@ -6,3 +6,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
             snake_cased_char_list.append(converted_character)
         else:
             snake_cased_char_list.append(char)
+    snake_cased_string = ''.join(snake_cased_char_list)
+    clean_snake_cased_string = snake_cased_string.strip('_')
+
+    return clean_snake_cased_string
