d47e623bfe8347a1ee11221f9cbebd4ed0bc15c5
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-26 22:57:09 +0500

    clearing uppercase

--- CaseConverter/main.py
+++ CaseConverter/main.py
@@ -1,2 +1,8 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
