a36c53a041f1114bf07028ca163cb4c1e790f1f5
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-28 00:02:04 +0500

    choice of a random element

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,6 +1,14 @@
+import random
 import string
+
 
 # Define the possible characters for the password
 letters = string.ascii_letters
 digits = string.digits
 symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
+
+print(all_characters)
+print(random.choice(all_characters))
