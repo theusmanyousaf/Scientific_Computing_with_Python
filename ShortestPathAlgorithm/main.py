11ffad66d8eed1b659bd9b7edd82ffc84f767c58
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-01 22:11:06 +0500

    dictionary comprehension

--- ShortestPathAlgorithm/main.py
+++ ShortestPathAlgorithm/main.py
@@ -6,14 +6,11 @@ my_graph = {
 }
 
 def shortest_path(graph, start):
-    unvisited = []
-    distances = {}
-    for node in graph:
-        unvisited.append(node)
-        if node == start:
-            distances[node] = 0
-        else:
-            distances[node] = float('inf')
-    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
     
 shortest_path(my_graph, 'A')
