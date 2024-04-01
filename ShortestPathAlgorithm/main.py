0d791e576062288fea205940670d631e37428965
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-01 22:14:04 +0500

    exploring the paths logic

--- ShortestPathAlgorithm/main.py
+++ ShortestPathAlgorithm/main.py
@@ -10,7 +10,11 @@ def shortest_path(graph, start):
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
-    
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
     print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
     
-shortest_path(my_graph, 'A')
+# shortest_path(my_graph, 'A')
