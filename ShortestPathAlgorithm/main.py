79d74e275c3fc81201daa06ff983460013599635
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-02 23:05:31 +0500

    fixed bugs

--- ShortestPathAlgorithm/main.py
+++ ShortestPathAlgorithm/main.py
@@ -16,8 +16,8 @@ def shortest_path(graph, start):
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
-                if paths[node][-1] == node:
-                    paths[node] = paths[current]
+                if paths[node] and paths[node][-1] == node:
+                    paths[node] = paths[current][:]
                 else:
                     paths[node].extend(paths[current])
                 paths[node].append(node)
@@ -25,4 +25,4 @@ def shortest_path(graph, start):
     
     print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
     
-# shortest_path(my_graph, 'A')
+shortest_path(my_graph, 'A')
