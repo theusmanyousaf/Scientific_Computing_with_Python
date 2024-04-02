09fd43b0c8a25472f2d2e6ae430f011c928ef820
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-02 23:04:00 +0500

    added paths logic

--- ShortestPathAlgorithm/main.py
+++ ShortestPathAlgorithm/main.py
@@ -10,11 +10,19 @@ def shortest_path(graph, start):
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
+    
     while unvisited:
         current = min(unvisited, key=distances.get)
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
+                if paths[node][-1] == node:
+                    paths[node] = paths[current]
+                else:
+                    paths[node].extend(paths[current])
+                paths[node].append(node)
+        unvisited.remove(current)
+    
     print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
     
 # shortest_path(my_graph, 'A')
