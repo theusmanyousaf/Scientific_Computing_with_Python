0653813954e9005743e273efd9123fcaee808451
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-03 23:58:12 +0500

    no of discs and rods for the puzzle

--- TowerOfHanoiPuzzle/main.py
+++ TowerOfHanoiPuzzle/main.py
@@ -1,5 +1,11 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+
 rods = {
-    'A': list(range(3, 0, -1)),
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
     'C': []
 }
+
+def move():
+    pass
