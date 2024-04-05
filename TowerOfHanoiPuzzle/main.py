0725c47694c95aa86ae0f692b2b389a7ac74351c
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-05 23:28:13 +0500

    basic logic completed

--- TowerOfHanoiPuzzle/main.py
+++ TowerOfHanoiPuzzle/main.py
@@ -7,24 +7,37 @@ rods = {
     'C': []
 }
 
+def make_allowed_move(rod1, rod2):    
+    forward = False
+    if not rods[rod2]:
+        forward = True
+    elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
+        forward = True      
+                
+    if forward:
+        print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+        rods[rod2].append(rods[rod1].pop())
+    else:
+        print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+        rods[rod1].append(rods[rod2].pop())
+    
+    # display our progress
+    print(rods, '\n')
+
 def move(n, source, auxiliary, target):
     # display starting configuration
-    print(rods)
+    print(rods, '\n')
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
-            forward = False
-            if not rods[target]:
-                forward = True
-            elif rods[source] and rods[source][-1] < rods[target][-1]:
-                forward = True
-            if forward:
-                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+            make_allowed_move(source, target)
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+            make_allowed_move(source, auxiliary)
         elif remainder == 0:
             print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+            make_allowed_move(auxiliary, target)
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
