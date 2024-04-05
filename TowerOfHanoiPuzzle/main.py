5fdf9a2dd9e53e322648328455fe9734592c72ef
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-05 23:24:45 +0500

    condition for all moves

--- TowerOfHanoiPuzzle/main.py
+++ TowerOfHanoiPuzzle/main.py
@@ -12,8 +12,12 @@ def move(n, source, auxiliary, target):
     print(rods)
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
-        if (i + 1) % 3 == 1:
+        if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
