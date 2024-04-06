d188ef76b8dd4b5e206da87df6c897ad854467ab
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-06 15:34:22 +0500

    full recursion

--- TowerOfHanoiPuzzle/main.py
+++ TowerOfHanoiPuzzle/main.py
@@ -1,20 +1,22 @@
-NUMBER_OF_DISKS = 4
-rods = {
-    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
-    'B': [],
-    'C': []
-}
+NUMBER_OF_DISKS = 5
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B: list[int] = []
+C: list[int] = []
 
 def move(n, source, auxiliary, target):
-    if n > 0:
-        # move n - 1 disks from source to auxiliary, so they are out of the way
-        move(n - 1, source, auxiliary, target)
+    if n <= 0:
+        return
+    # move n - 1 disks from source to auxiliary, so they are out of the way
+    move(n - 1, source, target, auxiliary)
         
-        # move the nth disk from source to target
-        rods[target].append(rods[source].pop())
+    # move the nth disk from source to target
+    target.append(source.pop())
         
-        # display our progress
-        print(rods, '\n')
+    # display our progress
+    print(A, B, C, '\n')
+        
+    # move the n - 1 disks that we left on auxiliary onto target
+    move(n - 1,  auxiliary, source, target)
               
 # initiate call from source A to target C with auxiliary B
-move(NUMBER_OF_DISKS, 'A', 'B', 'C')
+move(NUMBER_OF_DISKS, A, B, C)
