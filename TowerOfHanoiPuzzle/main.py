0433bbb58b9487a380941fc1bbd92d508aef1f35
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-06 15:25:53 +0500

    updated func for any no. of disks

--- TowerOfHanoiPuzzle/main.py
+++ TowerOfHanoiPuzzle/main.py
@@ -1,6 +1,5 @@
-NUMBER_OF_DISKS = 3
-number_of_moves = 2**NUMBER_OF_DISKS - 1
-
+NUMBER_OF_DISKS = 4
+number_of_moves = 2 ** NUMBER_OF_DISKS - 1
 rods = {
     'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
@@ -12,8 +11,7 @@ def make_allowed_move(rod1, rod2):
     if not rods[rod2]:
         forward = True
     elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
-        forward = True      
-                
+        forward = True              
     if forward:
         print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
         rods[rod2].append(rods[rod1].pop())
@@ -30,14 +28,22 @@ def move(n, source, auxiliary, target):
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
-            print(f'Move {i + 1} allowed between {source} and {target}')
-            make_allowed_move(source, target)
+            if n % 2 != 0:
+                print(f'Move {i + 1} allowed between {source} and {target}')
+                make_allowed_move(source, target)
+            else:
+                print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+                make_allowed_move(source, auxiliary)
         elif remainder == 2:
-            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
-            make_allowed_move(source, auxiliary)
+            if n % 2 != 0:
+                print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+                make_allowed_move(source, auxiliary)
+            else:
+                print(f'Move {i + 1} allowed between {source} and {target}')
+                make_allowed_move(source, target)
         elif remainder == 0:
             print(f'Move {i + 1} allowed between {auxiliary} and {target}')
-            make_allowed_move(auxiliary, target)
+            make_allowed_move(auxiliary, target)           
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
