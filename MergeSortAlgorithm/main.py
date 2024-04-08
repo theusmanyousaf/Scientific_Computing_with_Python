bc956375d3ded99345a2b0dcd437914b1e5af779
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-08 21:36:36 +0500

    right part & main array sorting

--- MergeSortAlgorithm/main.py
+++ MergeSortAlgorithm/main.py
@@ -15,3 +15,7 @@ def merge_sort(array):
         if left_part[left_array_index] < right_part[right_array_index]:
             array[sorted_index] = left_part[left_array_index]
             left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
