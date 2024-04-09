ea22dd8c07f3a3ac4d3b127a005292675babc4d8
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-04-09 21:15:54 +0500

    whileloop logic left part

--- MergeSortAlgorithm/main.py
+++ MergeSortAlgorithm/main.py
@@ -19,3 +19,9 @@ def merge_sort(array):
             array[sorted_index] = right_part[right_array_index]
             right_array_index += 1
         sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+    
