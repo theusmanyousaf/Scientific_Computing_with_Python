ea3ddd6a7703de6ce5439255ca7d9e50ba0af88a
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-05-03 21:02:43 +0500

    validation of a value in square logic

--- SudokuSolver/main.py
+++ SudokuSolver/main.py
@@ -44,3 +44,12 @@ class Board:
             self.board[row][col] != num
             for row in range(9)
         )
+    
+    def valid_in_square(self, row, col, num):
+        row_start = (row // 3) * 3
+        col_start=(col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == num:
+                    return False
+        return True
