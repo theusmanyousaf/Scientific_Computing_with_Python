8def67480ee5bfd565aed7ed48b2b166d239fba4
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-05-08 22:16:12 +0500

    search func logic

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -24,4 +24,8 @@ class BinarySearchTree:
         return self._search(self.root, key)
 
     def _search(self, node, key):
-        pass
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
