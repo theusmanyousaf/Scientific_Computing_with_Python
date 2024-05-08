334de1e071f4ed43f59e3ab490393b2345284644
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-05-08 22:14:30 +0500

    inster helper function logic

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -12,4 +12,10 @@ class BinarySearchTree:
         self.root = self._insert(self.root, key)
 
     def _insert(self, node, key):
-        pass
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
