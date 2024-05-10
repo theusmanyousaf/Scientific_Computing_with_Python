183d093e76f87e37d21d11ba4d0809acd8726553
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-05-10 23:35:21 +0500

    minor changes

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -4,25 +4,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return str(self.key)
+
 class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self,key):
-        self.root = self._insert(self.root, key)
-
     def _insert(self, node, key):
         if node is None:
             return TreeNode(key)
+
         if key < node.key:
             node.left = self._insert(node.left, key)
         elif key > node.key:
+
             node.right = self._insert(node.right, key)
         return node
-    
-    def search(self, key):
-        return self._search(self.root, key)
 
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+        
     def _search(self, node, key):
         if node is None or node.key == key:
             return node
@@ -30,8 +32,8 @@ class BinarySearchTree:
             return self._search(node.left, key)
         return self._search(node.right, key)
     
-    def delete(self, key):
-        self.root = self._delete(self.root, key)
+    def search(self, key):
+        return self._search(self.root, key)
 
     def _delete(self, node, key):
         if node is None:
@@ -49,4 +51,12 @@ class BinarySearchTree:
             node.key = self._min_value(node.right)
             node.right = self._delete(node.right, node.key)   
         
-        return node   
+        return node
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _min_value(self, node):
+        while node.left is not None:
+            node = node.left
+        return node.key
